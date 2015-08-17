be92181df027b75c4cf28812eb263812d9bd0ce7
Ashish Chandra <mail.ashishchandra@gmail.com>
2015-08-17 14:40:26 +0400

    Update for_loop.sh

--- for_loop.sh
+++ for_loop.sh
@@ -9,3 +9,26 @@ done
 exit 0
 *******************************************
 
+#!/bin/bash
+while true
+do
+    router_list=`ip netns | awk '/qr/ {print $0}'`
+    router_array=($router_list)
+
+    for router in ${router_array[@]};
+       do
+            ip_list=`ip netns exec $router ip a | awk '/qg/ {print $0}' | awk '/inet/ {print $2}'`
+            if [[ !  -z  $ip_list  ]]; then
+                 ip_array=($ip_list)
+                 #echo $router : ${ip_array[0]}
+                 oct1=$(echo ${ip_array[0]} | tr "." " " | awk '{ print $1 }')
+                 oct2=$(echo ${ip_array[0]} | tr "." " " | awk '{ print $2 }')
+                 oct3=$(echo ${ip_array[0]} | tr "." " " | awk '{ print $3 }')
+                 oct4=$(echo ${ip_array[0]} | tr "." " " | awk '{ print $4 }')
+                 gw_ip_addr=$oct1.$oct2.$oct3.1
+                 #echo $gw_ip_addr
+                 ip netns exec $router ping -c 1 $gw_ip_addr
+           fi
+       done
+       sleep 5
+done
