670bff2ee4b9e77b60e8c05b80329a3e04e0b2ef
Ashish Chandra <achandra@genesiscloud.com>
2019-01-02 16:42:16 +0100

    Revisted for loops

--- for_loop.sh
+++ for_loop.sh
@@ -1,13 +1,26 @@
+#!/bin/bash
 for file in $(ls a*.sh); do
    echo "$file"
 done
 exit 0
-******************************************
-for foo in seq 1 20
-echo " dude enough"
-done
-exit 0
 *******************************************
+## Print 1 to 100 in a sequence
+
+#!/bin/bash
+for i in {1..100}; do
+  echo $i
+done
+******************************************
+## Nested for  loop
+#!/bin/bash
+for i in a b c d e f g h; do
+    for j in 1 2 3 4 5 6 7 8; do
+        echo $i $j
+    done
+done
+
+*****************************************
+
 
 #!/bin/bash
 while true
