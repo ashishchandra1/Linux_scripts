a4c58dac2bb6758eb43cfa9db6cb23c31b94cae7
Ashish Chandra <achandra@genesiscloud.com>
2019-01-02 16:57:16 +0100

    Revisted if else while

--- if_else_while.sh
+++ if_else_while.sh
@@ -1,19 +1,28 @@
-echo " is it morning"
+# Check for the presence of variable, if not present set something else
+#!/bin/bash
+if [ -z $ghprbPullId ]; then
+   PRID="master"
+else
+  PRID=$ghprbPullId
+fi
+***************************************************
+# A typical use of if/ elif and else
+ #!/bin/bash
 
-read timeofday
-
-if [ " $timeofday" ="yes" ] ; then
-echo "Good Morning"
-
-elif ["$timeofday" ="no" ] ; then 
-echo "Bad Morning"
-
-exit 1
+echo "What is the PR Status(closed/ open) ?"
+read PR_STATUS
+if [ "$PR_STATUS" == "closed" ]; then
+    echo "********************** The Pull Request: $PRID is already merged, exiting the job **********************"
+    exit 1;
+elif [ "$PR_STATUS" == "open" ] || [ -z $PRID ]; then
+    :
+else
+   echo "********************** The PR Doesn't exists, exiting the job **********************"
+   exit 1;
 fi
 
-exit 0
-
 ***************************************************
+
 echo "Enter  password"
 read trythis
 
